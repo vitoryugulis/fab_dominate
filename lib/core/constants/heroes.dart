39a72252caeb9a2613f5f0164d92df9f74c4f19f
--replace-all <vitor.yugulis@caju.com.br>
2025-05-25 19:37:23 -0300

    feat: add file for heroes image links

---
+++ lib/core/constants/heroes.dart
@@ -0,0 +1,101 @@
+class HeroImages {
+  // High Seas
+  static const String gravyBones = '';
+  static const String puffin = '';
+  static const String marlynn = '';
+  static const String theHunted = '';
+  static const String cindra =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/cindra_cover.width-10000.format-webp.webp';
+  static const String fang =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/fang_story_1_Qw4CBaj.width-10000.format-webp.webp';
+  static const String arakni =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/chaos_story_1_74JOzBs.width-10000.format-webp.webp';
+  static const String slippy =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/slippy_cover.width-10000.format-webp.webp';
+
+  static const String other =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/sea_preview_cover.width-10000.format-webp.webp';
+
+  // 2024
+  static const String jarl =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/ad-origins_jarl_art.original.jpg';
+  static const String aurora =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/aurora_cover.width-10000.format-webp.webp';
+  static const String florian = '';
+  static const String oscilio =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/oscilio_cover.width-10000.format-webp.webp';
+  static const String verdance =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/verdance_cover.width-10000.format-webp.webp';
+  static const String terra = '';
+  static const String enigma =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/enigma_wide.width-10000.format-webp.webp';
+  static const String nuu =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/nuu_wide.width-10000.format-webp.webp';
+  static const String zen =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/mst_spoilers_2_66bJHBT.width-10000.format-webp.webp';
+  static const String betsy =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/hvy_titlescreen_betsy.width-10000.format-webp.webp';
+  static const String kassai =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/hvy_titlescreen_kassai_of_the_g.width-10000.format-webp.webp';
+  static const String kayo =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/hvy_titlescreen_kayo.width-10000.format-webp.webp';
+  static const String olympia =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/hvy_titlescreen_olympia.width-10000.format-webp.webp';
+  static const String victorGoldmane =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/hvy_titlescreen_victor.width-10000.format-webp.webp';
+
+  // 2023
+  static const String dash = '';
+  static const String maxx =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/M-BOOST_Moonshot_JessadaSutthi.width-992.format-webp.webp';
+  static const String teklovossen = '';
+  static const String brevant = '';
+  static const String melody = '';
+  static const String prism =
+      'https://mktg-assets.tcgplayer.com/content/opengraph/FAB-Prism-Awakener-of-Sol-DTD.jpg';
+  static const String vynnset =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/SHADOW-RUNEBLADE_HERO_Vynnset_Iro.width-992.format-webp.webp';
+  static const String riptide =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/riptide.width-992.format-webp.webp';
+  static const String uzuri = '';
+
+  // 2022
+  static const String emperor = '';
+  static const String yoji = '';
+  static const String dromai = '';
+  static const String fai = '';
+  static const String iyslander = '';
+  static const String bravo = '';
+  static const String genis = '';
+  static const String valda = '';
+
+  // 2021
+  static const String briar = '';
+  static const String lexi = '';
+  static const String oldhim = '';
+  static const String boltyn =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/bol_main_hero_image_001.width-992.format-webp.webp';
+  static const String chane = '';
+  static const String levia = '';
+
+  // 2020
+  static const String benji = '';
+  static const String dataDoll = '';
+  static const String kavdaen = '';
+  static const String shiyana = '';
+  static const String azalea =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/azalea.width-992.format-webp.webp';
+  static const String kano =
+      'https://www.cardtrader.com/uploads/blueprints/image/218176/show_kano-dracai-of-aether-marvel-history-pack-1-black-label.png';
+  static const String viserai = '';
+
+  // 2019
+  static const String dorinthea =
+      'https://www.cardtrader.com/uploads/blueprints/image/218174/show_dorinthea-ironsong-marvel-history-pack-1-black-label.png';
+  static const String katsu = '';
+  static const String rhinar =
+      'https://dhhim4ltzu1pj.cloudfront.net/media/images/hvy_titlescreen_rhinar.width-10000.format-webp.webp';
+
+  // 2018
+  static const String ira = '';
+}
