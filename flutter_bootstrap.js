1d41997073900aeaae310933f4c0de0974c6abcf
vitoryugulis <vitoryugulis@users.noreply.github.com>
2025-06-02 20:22:53 +0000

    deploy: 7d41bb874947e9b8f83939817a052c5f25416090

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3520573627"
+    serviceWorkerVersion: "66430581"
   }
 });
